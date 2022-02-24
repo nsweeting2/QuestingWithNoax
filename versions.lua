c266bb393a049bade17ba01ca89c9a26296f22ec
nsweeting2 <55107062+nsweeting2@users.noreply.github.com>
2022-02-24 14:51:08 -0500

    versions was backwards

---
+++ versions.lua
@@ -0,0 +1,4 @@
+-- ensure latest version listed here is the same one as in the script
+--current versino should be at index 1 oldest at the last index
+
+return {{[1] = [[2.1]],[2] = [[2.0]],[3] = [[1.1]],[4] = [[1.0]],},}
